29ec53e77e6f5065772af2508ddbc356353c1b38
Arc <atandarokib@gmail.com>
2023-08-08 22:59:12 +0100

    up

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,14 +7,8 @@ const About = () => {
       <img src={barack} alt="" className="mx-auto" />
 
       <div className="flex flex-col items-start gap-8">
-        <p className="text-white text-lg font-normal leading-[48px]">
-          Pepe and Toad the memecoin Launched on the ethereum. Our mission is to
-          provide a fun, community-driven investment opportunity that celebrates
-          the power of memes and pop culture. With a vision to become the
-          leading memecoin in the market, we believe Pepe and Toad is the
-          perfect ortfolio. Join us on this journey to revolutionize the world
-          of memecoins.
-        </p>
+        <img src={barack} alt="" className="mx-auto" />
+
         <a
           href=""
           className="px-[10.50px] pb-[3px] bg-gradient-to-r from-yellow-400 via-cyan-400 to-red-600 justify-center items-center flex"
